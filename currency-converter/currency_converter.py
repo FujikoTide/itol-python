af7046db5eed560b2956996cb4bd6799e7aac84d
Robin Mukherjee <robin@otaking.co.uk>
2025-08-14 15:28:34 +0100

    move function calls into main

--- currency-converter/currency_converter.py
+++ currency-converter/currency_converter.py
@@ -8,10 +8,3 @@ def convert_currency(amount: float, currency_from: str, currency_to: str) -> flo
         f"Converting {currency_from} to {currency_to}: {amount} {currency_from} is {converted_amount} {currency_to}."
     )
     return converted_amount
-
-
-convert_currency(100, "USD", "EUR")
-convert_currency(100, "JPY", "EUR")
-convert_currency(100, "USD", "GBP")
-convert_currency(100, "GBP", "JPY")
-convert_currency(100, "EUR", "USD")
--- currency-converter/main.py
+++ currency-converter/main.py
@@ -1,5 +1,12 @@
+from currency_converter import convert_currency
+
+
 def main():
-    pass
+    convert_currency(100, "USD", "EUR")
+    convert_currency(100, "JPY", "EUR")
+    convert_currency(100, "USD", "GBP")
+    convert_currency(100, "GBP", "JPY")
+    convert_currency(100, "EUR", "USD")
 
 
 if __name__ == "__main__":
